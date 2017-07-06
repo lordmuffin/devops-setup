02254a5538237aa7dfbef3f7c3a4b0cee056835a
Andrew Jackson <Andrew.Jackson@entrustdatacard.com>
2017-07-06 13:08:48 -0500

    Update cleanup.

--- autoinstall/basic-install.sh
+++ autoinstall/basic-install.sh
@@ -55,9 +55,9 @@ else
   brew upgrade
 fi
 
-if [ -d "./.yadr" ]; then
+if [ -d "~/.yadr" ]; then
   fancy_echo "YADR repo dir exists. Removing ..."
-  rm -rf ./.yadr
+  rm -rf ~/.yadr
 fi
 fancy_echo "YADR rake install..."
 git clone https://github.com/skwp/dotfiles.git ~/.yadr
@@ -65,9 +65,9 @@ cd ~/.yadr
 rake install
 
 # Clone the repository to your local drive.
-if [ -d "./Git" ]; then
+if [ -d "~/Git" ]; then
   fancy_echo "Laptop repo dir exists. Removing ..."
-  rm -rf ./Git
+  rm -rf ~/Git
 fi
 git clone https://github.com/lordmuffin/devops-setup.git ~/Git/devops-setup
 fancy_echo "Changing to laptop repo dir ..."
